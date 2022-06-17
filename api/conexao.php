d79a322e3a61404d851ff58a0a105f54c7b375e6
tamerfs <tamer.srhn@gmail.com>
2022-06-17 15:58:11 -0300

    tirado conexão com mysql para rodar no vercel

--- api/conexao.php
+++ api/conexao.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $hostname = "localhost"; // endereço do meu host seja local ou web exemplo mysql.hostgator.com
 $db = "gorgun_sql"; // nome do banco de dados criado/ tabela 
 $user = "root"; // para localhost e wamp usamos root padrão e senha em branco
@@ -8,9 +8,9 @@ $password = "";
 $mysqli = new mysqli($hostname, $user, $password, $db);
 
 $frase = "<footer>📚</footer>";
-
+*/
 if ($mysqli -> connect_errno){
-    echo "falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    echo "falha ao conectar: (mysql atrelado somente ao localhost" ;//. $mysqli->connect_errno . ") " . $mysqli->connect_error;
 } else
     echo $frase;
 
