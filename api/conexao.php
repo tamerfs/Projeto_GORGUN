dcce4cfafd0c66454ad03bec3b823c8f86afdb3f
TAMER  SERHAN <tamer.srhn@gmail.com>
2022-06-17 16:02:25 -0300

    Update conexao.php

--- api/conexao.php
+++ api/conexao.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $hostname = "localhost"; // endereço do meu host seja local ou web exemplo mysql.hostgator.com
 $db = "gorgun_sql"; // nome do banco de dados criado/ tabela 
 $user = "root"; // para localhost e wamp usamos root padrão e senha em branco
@@ -7,10 +7,10 @@ $password = "";
 
 $mysqli = new mysqli($hostname, $user, $password, $db);
 
-$frase = "<footer>📚</footer>";
-
+$frase = "📚";
+*/
 if ($mysqli -> connect_errno){
-    echo "falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    echo "falha ao conectar: (configurado somente como localhost no mysql)" //. $mysqli->connect_errno . ") " . $mysqli->connect_error;
 } else
     echo $frase;
 
